9d2a77d7720cebcdeaac0c3b4d90c856b3dfdf10
Timo Wischer <twischer@freenet.de>
2021-02-07 13:30:14 +0100

    Ensure that IRAM is mapped before boot_from_something_uart_dwnld()
    Same issue as before in case of esptool flasher stub
    
    TRACE +0.000 Received full packet: 01060200b5aa8c000000
    TRACE +0.000 Received full packet: 00000000
    A fatal error occurred: Failed to start stub. Unexpected response: b'\x00\x00\x00\x00'
    
    But expects
    
    TRACE +0.007 Read 1 bytes: c0
    TRACE +0.000 Read 17 bytes:
        01060200b5aa8c00 0000c0c04f484149 | ............OHAI
        c0                                | .
    TRACE +0.000 Received full packet: 01060200b5aa8c000000
    TRACE +0.000 Received full packet: 4f484149
    
    const uint32_t greeting = 0x4941484f; /* OHAI */
    of stub_flasher.c seems to be read as 0x00;
    
    Signed-off-by: Timo Wischer <twischer@freenet.de>

--- cores/esp8266/reboot_uart_dwnld.cpp
+++ cores/esp8266/reboot_uart_dwnld.cpp
@@ -27,6 +27,7 @@ void ICACHE_RAM_ATTR boot_from_something_uart_dwnld(void (**user_start_ptr)())
 	Uart_Init(0);
 	ets_install_uart_printf(0);
 
+	CLEAR_PERI_REG_MASK(PERIPHS_DPORT_IRAM_MAPPING, IRAM_UNMAP_40108000 | IRAM_UNMAP_4010C000);
 	boot_from_something_uart_dwnld(&user_start_fptr);
 
 	if (user_start_fptr == NULL) {
